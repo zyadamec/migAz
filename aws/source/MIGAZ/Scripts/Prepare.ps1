1b2e599ae69185b1d8e973a50c5078dea4415fbe
Paulo Ramos <paulo.ramos@live.com>
2017-01-27 14:21:32 +0000

    v1.0.0.1
    Adding Prepare.ps1 to handle the disks URL replacement and delete ASR
    resources automation

---
+++ aws/source/MIGAZ/Scripts/Prepare.ps1
@@ -0,0 +1,104 @@
+﻿# v1.0.0.1
+# Parameters
+param (
+    [Parameter(Mandatory = $true)] 
+    $ResourcegroupName,
+
+    [Parameter(Mandatory = $true)] 
+    $TemplateFilePath,
+
+    [ValidateSet("StopASRVirtualMachines", "DeleteASRVirtualMachines", "UpdateTemplateDisks")]
+    [Parameter(Mandatory = $true)] 
+    $Action,
+
+    $Sufix = ""
+)
+
+# If action is to update template disks
+If ($Action -eq "UpdateTemplateDisks")
+{
+    $disks = @()
+
+    $virtualmachines = Get-AzureRmVM -ResourceGroupName $ResourcegroupName
+    foreach ($virtualmachine in $virtualmachines)
+    {
+        $vmname = $virtualmachine.Name
+        $vmname = $vmname.Substring(0,$vmname.Length-$Sufix.Length)
+
+        $disk = New-Object System.Object
+        $disk | Add-Member -type NoteProperty -name vmname -value $vmname
+        $disk | Add-Member -type NoteProperty -name lun -value "OS"
+        $disk | Add-Member -type NoteProperty -name url -value $virtualmachine.StorageProfile.OsDisk.Vhd.Uri
+        $disks += $disk
+
+        foreach ($datadisk in $virtualmachine.StorageProfile.DataDisks)
+        {
+            $disk = New-Object System.Object
+            $disk | Add-Member -type NoteProperty -name vmname -value $vmname
+            $disk | Add-Member -type NoteProperty -name lun -value $datadisk.Lun
+            $disk | Add-Member -type NoteProperty -name url -value $datadisk.Vhd.Uri
+            $disks += $disk
+        }
+    }
+
+    $template = Get-Content $TemplateFilePath | ConvertFrom-Json
+    for($i=0; $i -lt $resources.resources.Count; $i++)
+    {
+        $resource = $template.resources[$i]
+        if ($resource.type -eq 'Microsoft.Compute/virtualMachines')
+        {
+            $template.resources[$i].properties.storageProfile.osDisk.vhd.uri = GetDiskUrl -vmname $resource.name -lun 'OS'
+
+            for($lunindex=0; $lunindex -lt $resource.properties.storageProfile.dataDisks.Count; $lunindex++)
+            {
+                $template.resources[$i].properties.storageProfile.dataDisks[$lunindex].vhd.uri = GetDiskUrl -vmname $resource.name -lun $lunindex.ToString()
+            }
+        }
+    }
+
+    $template | ConvertTo-Json -Depth 100 | Out-File $TemplateFilePath 
+}
+
+# If action is to stop ASR virtual machines
+If ($Action -eq "StopASRVirtualMachines")
+{
+    $resources = Get-Content $TemplateFilePath | ConvertFrom-Json
+    $virtualmachines = $resources.resources | ? type -EQ 'Microsoft.Compute/virtualMachines'
+    foreach ($virtualmachine in $virtualmachines)
+    {
+        $vmname = $virtualmachine.Name+$Sufix
+        Stop-AzureRmVM -ResourceGroupName $ResourcegroupName -Name ($vmname) -Force -StayProvisioned -Verbose
+    }
+}
+
+# If action is to delete ASR virtual machines and network interfaces
+If ($Action -eq "DeleteASRVirtualMachines")
+{
+    $resources = Get-Content $TemplateFilePath | ConvertFrom-Json
+    $virtualmachines = $resources.resources | ? type -EQ 'Microsoft.Compute/virtualMachines'
+    foreach ($virtualmachine in $virtualmachines)
+    {
+        $vmname = $virtualmachine.Name+$Sufix
+        $vm = Get-AzureRmVM -ResourceGroupName $ResourcegroupName -Name ($vmname)
+        $nics = $vm.NetworkProfile.NetworkInterfaces
+        Remove-AzureRmVM -ResourceGroupName $ResourcegroupName -Name ($vmname) -Force -Verbose
+
+        foreach ($nic in $nics)
+        {
+            $nicarray = $nic.Id.Split("/")
+            $nicname = $nicarray[$nicarray.Count-1]
+            Remove-AzureRmNetworkInterface -ResourceGroupName $ResourcegroupName -Name $nicname -Force -Verbose
+        }
+    }
+}
+
+Function GetDiskUrl($vmname, $lun)
+{
+    foreach($disk in $disks)
+    {
+        if ($disk.vmname -eq $vmname -and $disk.lun -eq $lun)
+        {
+            return $disk.url
+        }
+    }
+}
