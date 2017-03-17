36e2f2773aee86d2bd461026a640552584067290
rsanderson2350 <russell@russellanderson.net>
2017-03-17 08:16:45 -0600

    MigAz v2.0.0.0

---
+++ MigAz/Forms/UnhandledExceptionDialog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MigAz.Forms
+{
+    public partial class UnhandledExceptionDialog : Form
+    {
+        Exception _UnhandledException;
+
+        private UnhandledExceptionDialog() { }
+
+        public UnhandledExceptionDialog(Exception e)
+        {
+            InitializeComponent();
+            _UnhandledException = e;
+
+            textBox1.Text = e.Message + Environment.NewLine + e.StackTrace;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Azure/migAz/issues/new");
+        }
+    }
+}
--- MigAz/Program.cs
+++ MigAz/Program.cs
@@ -14,7 +14,14 @@ namespace MigAz
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += Application_ThreadException;
             Application.Run(new MigAzForm());
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            UnhandledExceptionDialog exceptionDialog = new UnhandledExceptionDialog(e.Exception);
+            exceptionDialog.ShowDialog();
         }
     }
 }
