4a77f0dab61d04921dd136017bd3fe255d5331e5
Russell Anderson <russell@russellanderson.net>
2017-05-29 23:27:38 -0600

    MigAz v2.2.5.0

--- MigAz/UserControls/Migrators/AsmToArm.cs
+++ MigAz/UserControls/Migrators/AsmToArm.cs
@@ -713,7 +713,7 @@ namespace MigAz.UserControls.Migrators
 
         private async Task AlertIfNewVersionAvailable()
         {
-            string currentVersion = "2.2.4.0";
+            string currentVersion = "2.2.5.0";
             VersionCheck versionCheck = new VersionCheck(this.LogProvider);
             string newVersionNumber = await versionCheck.GetAvailableVersion("https://api.migaz.tools/v1/version", currentVersion);
             if (versionCheck.IsVersionNewer(currentVersion, newVersionNumber))
