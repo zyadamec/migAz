349d8a53d64534591b3382e4a5447a5a6cb8d3c0
Russell Anderson <russell@russellanderson.net>
2018-01-20 11:32:18 -0700

    Reenable full target authentication change

--- MigAz/Migrators/AsmToArm.Designer.cs
+++ MigAz/Migrators/AsmToArm.Designer.cs
@@ -44,12 +44,12 @@
             // 
             // azureLoginContextViewerARM
             // 
-            this.azureLoginContextViewerARM.ChangeType = MigAz.Azure.UserControls.AzureLoginChangeType.SubscriptionOnly;
+            this.azureLoginContextViewerARM.ChangeType = MigAz.Azure.UserControls.AzureLoginChangeType.Full;
             this.azureLoginContextViewerARM.Enabled = false;
-            this.azureLoginContextViewerARM.Location = new System.Drawing.Point(452, 2);
-            this.azureLoginContextViewerARM.Margin = new System.Windows.Forms.Padding(1);
+            this.azureLoginContextViewerARM.Location = new System.Drawing.Point(678, 3);
+            this.azureLoginContextViewerARM.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.azureLoginContextViewerARM.Name = "azureLoginContextViewerARM";
-            this.azureLoginContextViewerARM.Size = new System.Drawing.Size(447, 106);
+            this.azureLoginContextViewerARM.Size = new System.Drawing.Size(670, 163);
             this.azureLoginContextViewerARM.TabIndex = 63;
             this.azureLoginContextViewerARM.Title = "Azure ARM (Target) Subscription";
             // 
@@ -57,10 +57,9 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(449, 109);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(674, 168);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(125, 13);
+            this.label6.Size = new System.Drawing.Size(186, 20);
             this.label6.TabIndex = 62;
             this.label6.Text = "Target (ARM) Resources";
             // 
@@ -68,9 +67,8 @@
             // 
             this.treeSourceASM.CheckBoxes = true;
             this.treeSourceASM.Location = new System.Drawing.Point(0, 0);
-            this.treeSourceASM.Margin = new System.Windows.Forms.Padding(2);
             this.treeSourceASM.Name = "treeSourceASM";
-            this.treeSourceASM.Size = new System.Drawing.Size(423, 262);
+            this.treeSourceASM.Size = new System.Drawing.Size(632, 401);
             this.treeSourceASM.TabIndex = 56;
             this.treeSourceASM.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeASM_AfterCheck);
             this.treeSourceASM.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeASM_AfterSelect);
@@ -78,10 +76,10 @@
             // azureLoginContextViewerASM
             // 
             this.azureLoginContextViewerASM.ChangeType = MigAz.Azure.UserControls.AzureLoginChangeType.Full;
-            this.azureLoginContextViewerASM.Location = new System.Drawing.Point(2, 2);
-            this.azureLoginContextViewerASM.Margin = new System.Windows.Forms.Padding(1);
+            this.azureLoginContextViewerASM.Location = new System.Drawing.Point(3, 3);
+            this.azureLoginContextViewerASM.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.azureLoginContextViewerASM.Name = "azureLoginContextViewerASM";
-            this.azureLoginContextViewerASM.Size = new System.Drawing.Size(447, 110);
+            this.azureLoginContextViewerASM.Size = new System.Drawing.Size(670, 169);
             this.azureLoginContextViewerASM.TabIndex = 64;
             this.azureLoginContextViewerASM.Title = "Azure Subscription";
             // 
@@ -91,21 +89,19 @@
             this.tabSourceResources.Controls.Add(this.tabPageArm);
             this.tabSourceResources.HotTrack = true;
             this.tabSourceResources.ImeMode = System.Windows.Forms.ImeMode.On;
-            this.tabSourceResources.Location = new System.Drawing.Point(2, 109);
-            this.tabSourceResources.Margin = new System.Windows.Forms.Padding(2);
+            this.tabSourceResources.Location = new System.Drawing.Point(3, 168);
             this.tabSourceResources.Name = "tabSourceResources";
             this.tabSourceResources.SelectedIndex = 0;
-            this.tabSourceResources.Size = new System.Drawing.Size(444, 300);
+            this.tabSourceResources.Size = new System.Drawing.Size(666, 462);
             this.tabSourceResources.TabIndex = 65;
             // 
             // tabPageAsm
             // 
             this.tabPageAsm.Controls.Add(this.treeSourceASM);
-            this.tabPageAsm.Location = new System.Drawing.Point(4, 22);
-            this.tabPageAsm.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPageAsm.Location = new System.Drawing.Point(4, 29);
             this.tabPageAsm.Name = "tabPageAsm";
-            this.tabPageAsm.Padding = new System.Windows.Forms.Padding(2);
-            this.tabPageAsm.Size = new System.Drawing.Size(436, 274);
+            this.tabPageAsm.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPageAsm.Size = new System.Drawing.Size(658, 429);
             this.tabPageAsm.TabIndex = 0;
             this.tabPageAsm.Text = "Source ASM Resources";
             this.tabPageAsm.UseVisualStyleBackColor = true;
@@ -113,11 +109,10 @@
             // tabPageArm
             // 
             this.tabPageArm.Controls.Add(this.treeSourceARM);
-            this.tabPageArm.Location = new System.Drawing.Point(4, 22);
-            this.tabPageArm.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPageArm.Location = new System.Drawing.Point(4, 29);
             this.tabPageArm.Name = "tabPageArm";
-            this.tabPageArm.Padding = new System.Windows.Forms.Padding(2);
-            this.tabPageArm.Size = new System.Drawing.Size(436, 274);
+            this.tabPageArm.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPageArm.Size = new System.Drawing.Size(658, 429);
             this.tabPageArm.TabIndex = 1;
             this.tabPageArm.Text = "Source ARM Resources";
             this.tabPageArm.UseVisualStyleBackColor = true;
@@ -126,9 +121,8 @@
             // 
             this.treeSourceARM.CheckBoxes = true;
             this.treeSourceARM.Location = new System.Drawing.Point(0, 0);
-            this.treeSourceARM.Margin = new System.Windows.Forms.Padding(2);
             this.treeSourceARM.Name = "treeSourceARM";
-            this.treeSourceARM.Size = new System.Drawing.Size(423, 262);
+            this.treeSourceARM.Size = new System.Drawing.Size(632, 401);
             this.treeSourceARM.TabIndex = 57;
             this.treeSourceARM.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeASM_AfterCheck);
             this.treeSourceARM.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeASM_AfterSelect);
@@ -136,28 +130,28 @@
             // treeTargetARM
             // 
             this.treeTargetARM.ImageList = null;
-            this.treeTargetARM.Location = new System.Drawing.Point(452, 131);
+            this.treeTargetARM.Location = new System.Drawing.Point(678, 202);
             this.treeTargetARM.LogProvider = null;
+            this.treeTargetARM.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.treeTargetARM.Name = "treeTargetARM";
             this.treeTargetARM.PropertyPanel = null;
             this.treeTargetARM.SelectedNode = null;
             this.treeTargetARM.SettingsProvider = null;
-            this.treeTargetARM.Size = new System.Drawing.Size(447, 262);
+            this.treeTargetARM.Size = new System.Drawing.Size(670, 403);
             this.treeTargetARM.StatusProvider = null;
             this.treeTargetARM.TabIndex = 66;
             // 
             // AsmToArm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.treeTargetARM);
             this.Controls.Add(this.tabSourceResources);
             this.Controls.Add(this.azureLoginContextViewerARM);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.azureLoginContextViewerASM);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "AsmToArm";
-            this.Size = new System.Drawing.Size(904, 418);
+            this.Size = new System.Drawing.Size(1356, 643);
             this.Load += new System.EventHandler(this.AsmToArmForm_Load);
             this.Resize += new System.EventHandler(this.AsmToArmForm_Resize);
             this.tabSourceResources.ResumeLayout(false);
