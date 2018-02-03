4ce3fdab0601ff055f1cb44e7150d8e8397d44bf
rsanderson2350 <russell@russellanderson.net>
2018-02-03 11:14:32 -0800

    MigAz

---
+++ MigAz.AzureStack/AzureStackContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigAz.AzureStack
+{
+    public class AzureStackContext
+    {
+
+        public async Task Login()
+        {
+            await this.TokenProvider.LoginAzureProvider();
+            UserAuthenticated?.Invoke(this);
+        }
+    }
+}
